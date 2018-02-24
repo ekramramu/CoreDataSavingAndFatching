6ce8dbf8c85bd1d6a3e6bcb46368c89b0ca011be
Ekramul Hoque <ekramulhoque@Ekramuls-Mac-mini.local>
2018-02-25 01:32:58 +0600

    Initial Commit

---
+++ CoreDataSavingAndFatching/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataSavingAndFatching
+//
+//  Created by Ekramul Hoque on 25/2/18.
+//  Copyright © 2018 Ekramul Hoque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
